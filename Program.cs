5f77ec03cfd02dd433214353c9b0e3bfb9a63c2d
Jamshidbek <jamshidbekmuxtarov@gmail.com>
2024-06-20 15:35:02 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Pharm
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Pharm");
+        }
+
+    }
+}
